73ce8914b3356f36c63120e9a859804632d016b5
Daniel Ben Avi <daniel.ben.avi10@gmail.com>
2024-08-11 11:16:54 +0300

    Initial Commit

---
+++ GymTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymTimer
+//
+//  Created by דניאל בן אבי on 11/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
