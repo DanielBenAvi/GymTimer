6eba6a4ea814f54672858b3300693fcbe3f1a7b4
Daniel Ben Avi <daniel.ben.avi10@gmail.com>
2024-08-14 11:08:06 +0300

    add image to exercise view

--- GymTimer/Lists/CustomExerciseInViewWorkoutCell.swift
+++ GymTimer/Lists/CustomExerciseInViewWorkoutCell.swift
@@ -11,9 +11,38 @@ import UIKit
 class CustomExerciseInViewWorkoutCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var numberOfSets: UILabel!
+    @IBOutlet weak var numberOfReps: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    @IBOutlet weak var breakTime: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    
+    
     
     func configure(with object: Exercise) {
         titleLabel.text = object.name
+        if object.image != "" {
+            icon.image = UIImage(named: object.image)
+        }
+        numberOfSets.text = "Sets: \(object.numberOfSets)"
+        numberOfReps.text = "Reps: \(object.numberOfReps)"
+        weight.text = "Weight: \(object.weight) Kg"
+        breakTime.text = "Break: \(object.breakTime) Sec"
+    }
+    
+    // Make the icon round
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        icon.layer.cornerRadius = icon.frame.size.width / 2
+        icon.clipsToBounds = true
+        
+        // Add border
+        icon.layer.borderWidth = 1.0
+        icon.layer.borderColor = UIColor(named: "MyPrimary")?.cgColor
+        
+        
+        // cell height
+        self.frame.size.height = 120
     }
 
 }
